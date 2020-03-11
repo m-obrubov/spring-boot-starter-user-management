59697baedfb458f80788871d0422b1d7b6f3edbc
Maxim Obrubov <31563836+m-obrubov@users.noreply.github.com>
2020-03-11 16:33:28 +0300

    Module template

---
+++ src/main/java/com/github/mobrubov/usermanagement/Application.java
@@ -0,0 +1,13 @@
+package com.github.mobrubov.springbootstarterusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterUserManagementApplication.class, args);
+	}
+
+}
